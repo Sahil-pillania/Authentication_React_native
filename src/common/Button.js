bd7ffadfc8915c766d5dea84881675e4398cdbde
Sahil pillania <sahilpillania88@gmail.com>
2023-01-23 12:09:42 +0530

    component

---
+++ src/common/Button.js
@@ -0,0 +1,5 @@
+export default module = {
+  button: {
+    backgoundColor: "#f50057",
+  },
+};
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import img from "../../assets/code.png";
+import module from "../common/Button";
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={styles.container_two}>
+        <Image style={styles.welImage} source={img} />
+      </View>
+      <View style={styles.container_form}></View>
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    // paddingTop: 170,
+    width: "100%",
+  },
+  container_two: {
+    width: "100%",
+    // height: "50%",
+    backgroundColor: "#fff",
+    position: "relative",
+    borderRadius: 15,
+    height: "50%",
+    // paddingTop: 200,
+    // alignItems: "left",
+  },
+
+  welImage: {
+    width: 350,
+    height: 60,
+    position: "absolute",
+    bottom: 180,
+    left: 25,
+    borderRadius: 14,
+  },
+  container_form: {
+    width: "100%",
+    height: "50%",
+
+    backgroundColor: "red",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+});
