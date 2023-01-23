f722f12f6fe6e69d6a9378428446dae2f60cab6b
Sahil pillania <sahilpillania88@gmail.com>
2023-01-23 13:33:17 +0530

    CSS

--- src/common/formcss.js
+++ src/common/formcss.js
@@ -1,5 +1,69 @@
-export default module = {
-  button: {
-    backgoundColor: "#f50057",
+const module = {
+  head1: {
+    fontSize: 30,
+    color: "black",
+    textAlign: "center",
+    // backgoundColor: "#f50057",
+  },
+  head2: {
+    fontSize: 20,
+    color: "#gray",
+    textAlign: "center",
+    // backgoundColor: "#f50057",
+  },
+  formgroup: {
+    // flex: 1,
+  },
+  label: {
+    fontSize: 16,
+    color: "#000",
+    marginLeft: 20,
+    marginBottom: 5,
+  },
+  input: {
+    fontSize: 16,
+    color: "#000",
+    borderRadius: 15,
+    padding: 10,
+    // borderWidth: 1,
+    margin: 15,
+    backgroundColor: "#eeb000",
+  },
+  input1: {
+    fontSize: 16,
+    color: "#000",
+    borderRadius: 15,
+    padding: 10,
+    // borderWidth: 1,
+    margin: 15,
+    height: 130,
+    backgroundColor: "#eeb000",
+  },
+  link: {
+    color: "black",
+    fontSize: 12,
+  },
+  fp: {
+    display: "flex",
+    // alignItems: "flex-end",
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  buttonCont: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  button1: {
+    width: "90%",
+    marginHorizontal: 10,
+    marginVertical: 10,
+    backgroundColor: "green",
+    padding: 5,
+    borderRadius: 15,
+    fontSize: 25,
+    textAlign: "center",
+    zIndex: 1,
   },
 };
+export default module;
