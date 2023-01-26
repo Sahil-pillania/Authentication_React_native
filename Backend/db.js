5828d71cac215bd248e20d721496d8dd5baf8c24
Sahil pillania <sahilpillania88@gmail.com>
2023-01-26 10:39:50 +0530

    db strict query

--- Backend/db.js
+++ Backend/db.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
+    mongoose.set("strictQuery", false);
     console.log("Connected to database");
   })
   .catch((err) => console.log("Couldn't connect to database", err));
