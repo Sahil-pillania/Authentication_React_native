731958d4d54d20a7d5c95ee8343f00d37a137fc3
Sahil pillania <sahilpillania88@gmail.com>
2023-01-27 12:33:14 +0530

    page

---
+++ src/screens/Verification.js
@@ -0,0 +1,51 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+import React from "react";
+import module from "../common/formcss";
+
+const Verification = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      <Text style={module.head1}>Verification page</Text>
+      <View style={module.buttonCont}>
+        <TouchableOpacity style={styles.buttonContTouch}>
+          <Text
+            style={module.button2}
+            onPress={() => {
+              navigation.navigate("login");
+            }}
+          >
+            Logout
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Verification;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    // paddingTop: 170,
+    // width: "100%",
+  },
+  buttonContTouch: {
+    alignItems: "center",
+    width: "100%",
+    // flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+    // left: 40,
+  },
+});
